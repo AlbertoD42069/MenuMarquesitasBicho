8e9052cc59f32e54c38f02ba19ef30df73175519
Jesus Alberto Diaz Dominguez <j.albertho123@gmail.com>
2024-09-16 12:19:38 -0600

    comentario

--- verMenu.js
+++ verMenu.js
@@ -10,6 +10,8 @@ function verMenu(){
 
     }
 }
+
+/* funcion para hacer scroll interno dentro de una view */
 function scrollToElement(event) {
     const scrollTarget = event.target.getAttribute('data-scroll');
     const scrollElement = document.querySelector(`[id-div="${scrollTarget}"]`);
